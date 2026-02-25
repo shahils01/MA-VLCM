c125f38f466214ee7a68b88e2782dbc2f9dea361
shahils01 <shahil.shaik7@gmail.com>
2026-02-25 13:03:37 -0500

    no major update

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,19 +1,19 @@
-TRAIN_SHARDS="/scratch/shahils/data/gotogoal_pt_0/shard-{000000..000110}.tar::/scratch/shahils/data/gotogoal_pt_15/shard-{000000..000110}.tar::/scratch/shahils/data/gotogoal_pt_30/shard-{000000..000110}.tar::/scratch/shahils/data/gotogoal_pt_45/shard-{000000..000110}.tar::/scratch/shahils/data/gotogoal_pt_225/shard-{000000..000110}.tar"
+TRAIN_SHARDS="/scratch/shahils/data/gotogoal_pt_0/shard-{000000..000285}.tar::/scratch/shahils/data/gotogoal_pt_15/shard-{000000..000287}.tar::/scratch/shahils/data/gotogoal_pt_30/shard-{000000..000159}.tar::/scratch/shahils/data/gotogoal_pt_45/shard-{000000..000129}.tar::/scratch/shahils/data/gotogoal_pt_225/shard-{000000..000111}.tar"
 VL_MODEL_PRESET="${VL_MODEL_PRESET:-llava_onevision_0p5b}"  # llava_onevision_0p5b / llava_next_video_7b
 
-accelerate launch --num_processes 2 train.py \
+accelerate launch --num_processes 6 train.py \
   --train_shards $TRAIN_SHARDS \
-  --batch_size 4 \
-  --num_workers 2 \
-  --grad_accum_steps 1 \
+  --batch_size 2 \
+  --num_workers 16 \
+  --grad_accum_steps 16 \
   --mixed_precision bf16 \
   --disable_vl_cache \
   --allow_tf32 \
   --value_pooling last_token_logits \
   --vl_logits_to_keep 1 \
   --epochs 500 \
-  --clip_len 25 \
-  --clip_stride 25 \
+  --clip_len 20 \
+  --clip_stride 20 \
   --robot_source obs \
   --reward_reduce mean \
   --done_reduce any \
@@ -28,9 +28,9 @@ accelerate launch --num_processes 2 train.py \
   The goal for each robot is denoted by the same color square box. The robots have to go to their designated goal \
   without colliding with one another. They also have to be efficient by taking the shortest parth. How Good or Bad are \
   the team of robots doing to accomplish the given task?" \
-  --peft lora \
-  --lora_r 8 \
-  --lora_alpha 16 \
+  --peft qlora \
+  --lora_r 16 \
+  --lora_alpha 32 \
   --lora_dropout 0.05 \
   --wandb \
   --wandb_project ma-vlcm \
