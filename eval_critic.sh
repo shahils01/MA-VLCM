fcacdfbb397e6833aa05a6d4cde1e0f5e0eb01dc
shahils01 <shahils@clemson.edu>
2026-03-13 14:40:47 -0400

    modified hyperparams

--- eval_critic.sh
+++ eval_critic.sh
@@ -1,11 +1,12 @@
 python eval_critic.py \
   --eval_shards "/scratch/shahils/data/gotogoal_new_pt_45/shard-{000281..000289}.tar" \
+  --vl_model_preset internvl3_5_4b \
   --batch_size 2 \
   --num_workers 2 \
   --max_samples 20 \
   --print_samples 20 \
   --value_pooling last_token_logits \
-  --checkpoint checkpoints/ckpt_epoch_2.pt \
+  --checkpoint checkpoints_new/ckpt_epoch_2.pt \
 
 
 
